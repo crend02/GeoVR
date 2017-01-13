d3fe0302db98214354e84e272538c0113cc6e542
noerw <noerw@gmx.de>
2017-01-13 11:24:09 +0100

    fuck vue
    had issues with aframe visible component :^(

--- src/main.js
+++ src/main.js
@@ -1,27 +1,5 @@
-import Vue from 'vue';
-import App from './App.vue';
+import AFRAME from './aframe-master.js';
+import teleport from 'aframe-teleport-controls';
+import cursor from 'aframe-vive-cursor-component';
 
-// vue will try to interpret the aframe entities as vue components
-// we don't want that. https://github.com/vuejs/vue/issues/3090
-Vue.config.ignoredElements = [
-  'a-sky',
-  'a-entity',
-  'a-box',
-  'a-sphere',
-  'a-plane',
-  'a-cylinder',
-  'a-node',
-  'a-scene',
-  'a-mixin',
-  'a-assets',
-  'a-camera',
-  'a-cursor',
-  'a-collada-model',
-  'a-asset-item',
-  //'a-entity',
-];
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
+import aframeComponents from './aframe_components.js';
