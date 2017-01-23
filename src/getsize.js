f0d6c1f5b1a69762ec855b8d0de8546d741f18db
noerw <noerw@gmx.de>
2017-01-23 15:56:21 +0100

    fix

--- src/getsize.js
+++ src/getsize.js
@@ -8,9 +8,16 @@ AFRAME.registerComponent('getsize', {
   update (oldData) {
     setTimeout(() => {
       const bbox = new THREE.Box3().setFromObject(this.el.object3D);
-      //bbox.update(this.el.object3D)
       const xDiff = Math.abs(bbox.min.x) + Math.abs(bbox.max.x);
-      console.log(this.data, xDiff, 1/xDiff);
+      const yDiff = Math.abs(bbox.min.y) + Math.abs(bbox.max.y);
+      const stats = {
+        component: this.data,
+        xDiff,
+        yDiff,
+        xDiffInverted: 1/xDiff,
+        yDiffInverted: 1/yDiff
+      }
+      console.log(stats);
     }, 3000)
   }
 });
