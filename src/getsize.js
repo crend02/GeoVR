45886e0c4d1f94845e38c3ee30feedc30808e1b4
noerw <noerw@gmx.de>
2017-01-24 11:06:25 +0100

    finally get the model sizes correct

--- src/getsize.js
+++ src/getsize.js
@@ -4,20 +4,30 @@ import AFRAME from './aframe-master.js'
 
 AFRAME.registerComponent('getsize', {
   schema: {default: "thing" },
-  // aframe lifecycle hook: called when component attributes have been changed
   update (oldData) {
+    // timeout required for the models to be loaded
     setTimeout(() => {
       const bbox = new THREE.Box3().setFromObject(this.el.object3D);
       const xDiff = Math.abs(bbox.min.x) + Math.abs(bbox.max.x);
       const yDiff = Math.abs(bbox.min.y) + Math.abs(bbox.max.y);
+      const zDiff = Math.abs(bbox.min.z) + Math.abs(bbox.max.z);
       const stats = {
         component: this.data,
-        xDiff,
-        yDiff,
-        xDiffInverted: 1/xDiff,
-        yDiffInverted: 1/yDiff
-      }
-      console.log(stats);
+        size: {
+          x: xDiff,
+          y: yDiff,
+          z: zDiff
+        },
+        scale: {
+          x: 1 / xDiff,
+          y: 1 / yDiff,
+          z: 1 / zDiff
+        }
+      };
+      // use size of shortest edge for scaling, so the shortest edge is always 1
+      const s = Math.max(stats.scale.x, stats.scale.z);
+      stats.scaleString = `id='${this.data}' scale='${[s,s,s].join(' ')}' `
+      console.log(JSON.stringify(stats, null, 2));
     }, 3000)
   }
 });
