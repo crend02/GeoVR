35ddcc4c2166c8501f74c63326057deb2b3cfd41
noerw <noerw@gmx.de>
2017-02-16 17:11:29 +0100

    extended toggle-view.js :tiger:

--- src/toggle-views.js
+++ src/toggle-views.js
@@ -5,16 +5,19 @@ AFRAME.registerComponent('toggle-views', {
   schema: { 
     view1: { type: 'selector', default: '#buildingview' },
     view2: { type: 'selector', default: '#imageview' },
+	view3: { type: 'selector', default: '#mview'}
   },
 
   init () {
     this.el.addEventListener('triggerdown', (ev) => {
-      if (this.data.view1.getAttribute('visible')) {
+      if (this.data.view1.getAttribute('visible')) {  
         this.data.view1.setAttribute('visible', 'false');
         this.data.view2.setAttribute('visible', 'true');
+		this.data.view3.setAttribute('visible', 'false');
       } else {
         this.data.view1.setAttribute('visible', 'true');
         this.data.view2.setAttribute('visible', 'false');
+		this.data.view3.setAttribute('visible', 'true');
       }
     });
   }
